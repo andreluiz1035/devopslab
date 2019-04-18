8fb8269406b047f364e9831c99b1f22e8a08ce69
andreluiz1035 <andreluiz.mcse@gmail.com>
2019-04-18 21:38:33 +0000

    v1.4

--- setup_worker.sh
+++ setup_worker.sh
@@ -1,49 +1,83 @@
 #!/bin/bash
 
-echo "########################################################################################"
-echo "  "
+HEADER_VAR="##############################################################################################"
+BLANK_VAR=" "
+
+echo "$HEADER_VAR"
+echo "$BLANK_VAR "
 echo "STEP: Install SSH"
-echo "  "
-echo "########################################################################################"
+echo "$BLANK_VAR"
+echo "$HEADER_VAR"
 
 apt install -y ssh
 if [ $? -eq 0 ]; then
-	echo "SSH installed"
+	SSH_VAR="SSH installed"
+	
 else
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	echo "Problemas instalando o SSH - Favor verificar" 
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	exit
 fi
 
-echo "#########################################################################################"
-echo "  "
-echo "STEP: Disable Ubuntu Firewall"
-echo "  "
-echo "#########################################################################################"
+echo "$HEADER_VAR"
+echo "$BLANK_VAR "
+echo "STEP: DISABLE UBUNTU FIREWALL"
+echo "$BLANK_VAR"
+echo "$HEADER_VAR"
 
 ufw disable
 if [ $? -eq 0 ]; then
-	echo "IPTABLES disabled"
+	IPTABLES_VAR="IPTABLES disabled"
 else
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	echo "Problemas desabilitando o IPTABLES - Favor verificar"
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	exit
 fi
 
-echo "#########################################################################################"
-echo "  "
-echo "STEP: SUDO WITHOUT PASSWD"
-echo "  "
-echo "#########################################################################################"
+echo "$HEADER_VAR"
+echo "$BLANK_VAR "
+echo "STEP: SUDO WITHOUT PASWWORD"
+echo "$BLANK_VAR"
+echo "$HEADER_VAR"
 
 sed  -i '/%sudo/a %sudo   ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
 
 if [ $? -eq 0 ]; then
-        echo "SUDO FREE"
+        SUDO_VAR="SUDO FREE"
 else
-        echo "Problemas com sudo"
-        exit
+        echo "@@@@@@@@@@@@@@@@@@@@@"
+	echo "Problemas com sudo"
+        echo "@@@@@@@@@@@@@@@@@@@@@"
+	exit
 fi
 
+echo "$HEADER_VAR"
+echo "$BLANK_VAR "
+echo "STEP: INSTALL PYTHON2"
+echo "$BLANK_VAR"
+echo "$HEADER_VAR"
 
+
+apt install -y python-minimal
+
+if [ $? -eq 0 ]; then
+        PYTHON_VAR="PYTHON INSTALLED"
+else
+        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	echo "Problemas para instalar o Python 2. Favor verificar"
+        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	exit
+fi
+
+echo "$BLANK_VAR"
+echo "OUTPUT:"
+echo "$BLANK_VAR"
+echo "$SSH_VAR"
+echo "$IPTABLES_VAR"
+echo "$SUDO_VAR"
+echo "$PYTHON_VAR"
 
 
 
