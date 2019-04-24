185e8a8709910e8db58cf31924f9efc48de62641
andreluiz1035 <andreluiz.mcse@gmail.com>
2019-04-24 16:25:46 +0000

    v2.1

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Author: Andre Luiz Alves de Souza
+#DATE: 04/2019
+#OBBJECTIVE: INSTALL ANSIBLE ON A MANAGER HOST AND CONFIGURE A MANAGED
+#HOST.
 
-HEADER_VAR="##############################################################################################"
+HEADER_VAR="###############################################################"
 BLANK_VAR=" "
 
 echo "$HEADER_VAR"
@@ -10,6 +13,7 @@ echo "STEP: Install SSH"
 echo "$BLANK_VAR"
 echo "$HEADER_VAR"
 
+#SSH Instalation
 apt install -y ssh
 if [ $? -eq 0 ]; then
         SSH_VAR="SSH installed"
@@ -27,13 +31,13 @@ echo "STEP: DISABLE UBUNTU FIREWALL"
 echo "$BLANK_VAR"
 echo "$HEADER_VAR"
 
+#Disabling Ubuntu Firewall
 ufw disable
 if [ $? -eq 0 ]; then
         IPTABLES_VAR="IPTABLES disabled"
 else
-        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        
         echo "Problemas desabilitando o IPTABLES - Favor verificar"
-        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
         exit
 fi
 
@@ -43,7 +47,7 @@ echo "STEP: GENERATE SSH KEY PAIR"
 echo "$BLANK_VAR"
 echo "$HEADER_VAR"
 
-
+#Generating SSH Keys
 (ls /root/.ssh/id_rsa && ls /root/.ssh/id_rsa.pub) >> /dev/null
 if [ $? != 0 ]; then
 	ssh-keygen
@@ -54,36 +58,50 @@ fi
 if [ $? -eq 0 ]; then
         SSHKEYS_VAR="Keys Successfull generated"
 else
-        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        
         echo "Problems creating ssh keys"
-        echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
         exit
 fi
 
+#Adding hosts file entries - Optional
 read -p "Do you want to add hosts file entries ? If Yes, type 1: (If you have consistent DNS name resolution, you dont need to edit hosts file)." ANSWARE_VAR
 if [ $ANSWARE_VAR -eq 1 ]; then
 	read -p "Please, insert the hostname of your k8s node :" MASTER_VAR
         read -p "Please, insert the ip address of you k8s node :" MASTERIP_VAR
         echo "$MASTERIP_VAR   $MASTER_VAR" >> /etc/hosts
+	if [ $? -eq 0 ]; then
+		FILEHOSTS_VAR = "Entrada adicionada com sucesso."
+	else
+		echo "Problems adding entries to /etc/hosts file."
+	fi
 fi
 
-
+#Copying SSH Keys
 read -p "Do you want to copy your SSH Keys to the K82 Server? If Yes, type 1: " ANSWARE_VAR
 if [ $ANSWARE_VAR -eq 1 ]; then
 	read -p "Please, enter the sudo enabled user :" USER_VAR
 	read -p "Please, enter the hostaname of you k8s node :" K8S_VAR
 	ssh-copy-id -i ~/.ssh/id_rsa.pub $USER_VAR@$K8S_VAR
+	if [ $? -eq 0 ]; then
+		$SSHCOPY_VAR = "SSH Keys copied."
+	else
+		echo "Problemas copiando chave SSH. Favor verificar"
+		exit
+	fi
 fi
 
+#Adding ansible repository
 echo "$HEADER_VAR"
 echo "$BLANK_VAR "
 echo "STEP: ADD ANSIBLE REPOSITORY"
 echo "$BLANK_VAR"
 echo "$HEADER_VAR"
 
-apt-add-repository ppa:ansible/ansible
+apt-add-repository -y ppa:ansible/ansible
 apt update
 
+
+#Installing ansible
 echo "$HEADER_VAR"
 echo "$BLANK_VAR "
 echo "STEP: INSTALL ANSIBLE"
@@ -91,12 +109,17 @@ echo "$BLANK_VAR"
 echo "$HEADER_VAR"
 apt install -y ansible
 if [ $? -eq 0 ]; then
-	echo "Ansible instalado com sucesso."
+	$ANSIBLE_VAR = "Ansible successfully installed."
 else
-	echo "problemas instalando ansible."
+	echo "Problemas instalando ansible. Favor verificar."
+	exit
 fi
 
-
-
-
+echo "RESULTS:"
+echo "$SSH_VAR"
+echo "$IPTABLES_VAR"
+echo "$SSHKEYS_VAR"
+echo "$FILEHOSTS_VAR"
+echo "$SSHCOPY_VAR"
+echo "$ANSIBLE_VAR"
 
